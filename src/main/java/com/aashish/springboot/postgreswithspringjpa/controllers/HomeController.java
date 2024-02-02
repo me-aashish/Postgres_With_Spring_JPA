ec541b2e088e94ad6d50872c7aa013d0f893be90
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-02-02 10:59:25 +0530

    created end point to add city

--- src/main/java/com/aashish/springboot/postgreswithspringjpa/controllers/HomeController.java
+++ src/main/java/com/aashish/springboot/postgreswithspringjpa/controllers/HomeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aashish.springboot.postgreswithspringjpa.entities.City;
@@ -19,5 +21,10 @@ public class HomeController {
 	public List<City> getCities() {
 
 		return cityService.getCities();
+	}
+
+	@PostMapping("addcity")
+	public City saveCity(@RequestBody City city) {
+		return cityService.saveCity(city);
 	}
 }
--- src/main/java/com/aashish/springboot/postgreswithspringjpa/services/CityService.java
+++ src/main/java/com/aashish/springboot/postgreswithspringjpa/services/CityService.java
@@ -17,4 +17,8 @@ public class CityService {
 	public List<City> getCities() {
 		return cityRepository.findAll();
 	}
+
+	public City saveCity(City city) {
+		return cityRepository.save(city);
+	}
 }
